48aee0190155c761d0f0a2982d981fa166c9dd16
Ravil <lllx2@yandex.ru>
2023-01-19 11:47:56 +0300

    2

--- Task 2.py
+++ Task 2.py
@@ -6,8 +6,54 @@
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота ""интеллектом""
 
-sum = 2021
-i = 0
-j = 0
-def first_step ():
+import random
+
+sum = 121
+step = 0
+player1 = 'Ivan'
+player2 = 'Dima'
+sum_player1 = 0
+sum_player2 = 0
+
+def take_candys(player):
+    t_c = int(input(f'{player}, возьмите не более чем 28 конфет, взял '))
+    while t_c < 1 or t_c > 28:
+        t_c = int(input(f'{player}, возьмите корректное количество конфет '))
+    return t_c 
+
+def p_print (sum_player, player, sum):
+    print(f'{player} всего {sum_player} конфет')
+    print(f'Осталось конфет {sum}')
+
+def game (sum_player, step, sum, candy):
     
+    sum_player += candy
+    sum -= candy
+    step += 1
+    return sum_player, step, sum
+
+flag = random.randint(1, 3)
+if flag:
+    print(f'Первым ходит {player1}')
+else:
+    print(f'Первым ходит {player2}')
+
+while sum > 28:
+    if flag:
+        candy = take_candys(player1)
+        sum_player1, step, sum = game (sum_player1, step, sum, candy)
+        flag = False
+        p_print(sum_player1, player1, sum)
+    else:
+        candy = take_candys(player2)
+        sum_player2, step, sum = game (sum_player2, step, sum, candy)
+        flag = True
+        p_print(sum_player2, player2, sum)
+        print(f'Для выигрыша первый игрок должен взять {random.randrange(1, 28, 2) + sum % 2} конфет')
+
+if flag:
+    sum_player1 += sum
+    print(f'Выиграл {player1}, т.к. забрал последние {sum} конфет, всего {sum_player1 + sum_player2} конфет')
+else:
+    sum_player2 += sum
+    print(f'Выиграл {player2}, т.к. забрал последние {sum} конфет, всего {sum_player2 + sum_player1} конфет')   
